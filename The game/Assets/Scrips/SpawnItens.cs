d1024ae15ab8081d9bcdf2463af3b1c4ee7fd7f7
YANN DE AQUINO FERREIRA JAYME <20221170150006@ifrn.local>
2023-10-11 10:19:44 -0300

    spawner

---
+++ The game/Assets/Scrips/SpawnItens.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItens : MonoBehaviour
+{
+    public GameObject[] SpawnItems;
+
+    private int random;
+
+    public float spawnTime;
+
+    public float spawnDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandom", spawnTime, spawnDelay);
+    }
+
+    // Update is called once per frame
+    void SpawnRandom()
+    {
+        random = Random.Range(0, SpawnItems.Length);
+        Instantiate(SpawnItems[random], transform.position, transform.rotation);
+    }
+}
