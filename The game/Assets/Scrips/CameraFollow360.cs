0ff551cc301caef44d6c0ae96fa0d29c7d1b9869
YANN DE AQUINO FERREIRA JAYME <20221170150006@ifrn.local>
2023-10-18 09:59:39 -0300

    Correção de coisas

--- The game/Assets/Scrips/CameraFollow360.cs
+++ The game/Assets/Scrips/CameraFollow360.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CameraFollow360 : MonoBehaviour
@@ -8,11 +9,13 @@ public class CameraFollow360 : MonoBehaviour
     private Vector3 offset;
     public float sensib = 2;
     private float yaw = 0;
+    private float pitch = 0;
     // Start is called before the first frame update
     void Start()
     {
         alvo = GameObject.FindWithTag("Player").transform;
         offset = alvo.position - transform.position;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -20,6 +23,7 @@ public class CameraFollow360 : MonoBehaviour
     {
         transform.position = alvo.position - offset;
         yaw += sensib * Input.GetAxis("Mouse X");
-        transform.eulerAngles = new Vector3(0, yaw, 0);
+        pitch -= sensib * Input.GetAxis("Mouse Y");
+        transform.eulerAngles = new Vector3(pitch, yaw, 0);
     }
 }
